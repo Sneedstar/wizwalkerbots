5680fcb642448931ea9d30d05eb2a9ebebf6e5b6
MajorPain1 <51036268+MajorPain1@users.noreply.github.com>
2021-09-12 15:11:08 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import asyncio
+from wizwalker.client import *
 from wizwalker.constants import Keycode
-from wizwalker.extensions.wizsprinter.sprinty_client import MemoryReadError
 
 potion_ui_buy = [
     "fillallpotions",
@@ -12,23 +12,24 @@ potion_ui_buy = [
     "exit"
 ]
 
+async def safe_click_window_with_name(client, button):
+    try:
+        await client.mouse_handler.click_window_with_name(button)
+    except ValueError:
+        await safe_click_window_with_name(client, button)
+
 async def logout_and_in(client):
     print(f'[{client.title}] Logging out and in')
     await asyncio.sleep(0.6)
     await client.send_key(Keycode.ESC, 0.1)
     await asyncio.sleep(0.4)
-    try:
-        await client.mouse_handler.click_window_with_name('QuitButton')
-    except ValueError:
-        await client.send_key(Keycode.ESC, 0.1)
-        await asyncio.sleep(0.4)
-        await client.mouse_handler.click_window_with_name('QuitButton')
+    await safe_click_window_with_name(client, 'QuitButton')
     await asyncio.sleep(0.4)
-    if await client.root_window.get_windows_with_name('centerButton'):
-        await asyncio.sleep(0.15)
+    try:
         await client.mouse_handler.click_window_with_name('centerButton')
-    await asyncio.sleep(8)
-    await client.mouse_handler.click_window_with_name('btnPlay')
+    except ValueError:
+        await asyncio.sleep(0.01)
+    await safe_click_window_with_name(client, 'btnPlay')
     await client.wait_for_zone_change()
 
 async def go_through_dialog(client):
@@ -48,9 +49,9 @@ async def auto_buy_potions(client):
     await client.send_key(Keycode.X, 0.1)
     await asyncio.sleep(1.4)
     # Go to Wizard City
-    await client.mouse_handler.click_window_with_name('wbtnWizardCity')
+    await safe_click_window_with_name(client, 'wbtnWizardCity')
     await asyncio.sleep(0.15)
-    await client.mouse_handler.click_window_with_name('teleportButton')
+    await safe_click_window_with_name(client, 'teleportButton')
     await client.wait_for_zone_change()
     # Walk to potion vendor
     await client.goto(-0.5264079570770264, -3021.25244140625)
@@ -68,7 +69,7 @@ async def auto_buy_potions(client):
     await asyncio.sleep(0.2)
     # Buy potions
     for i in potion_ui_buy:
-        await client.mouse_handler.click_window_with_name(i)
+        await safe_click_window_with_name(client, i)
         await asyncio.sleep(0.1)
     # Return
     await client.send_key(Keycode.PAGE_UP, 0.1)
@@ -80,11 +81,12 @@ async def safe_tp_to_mana(client):
     await client.tp_to_closest_mana_wisp()
   except MemoryReadError:
     await safe_tp_to_mana(client)
+    
 async def safe_tp_to_health(client):
   try:
     await client.tp_to_closest_health_wisp()
   except MemoryReadError:
-    await safe_tp_to_health(client)
+    await safe_tp_to_health(client) 
 
 async def collect_wisps(client):
     # Head to home world gate
@@ -96,11 +98,11 @@ async def collect_wisps(client):
     await asyncio.sleep(0.5)
     # Go to Mirage
     for i in range(3):
-        await client.mouse_handler.click_window_with_name('rightButton')
+        await safe_click_window_with_name(client, 'rightButton')
     await asyncio.sleep(0.1)
-    await client.mouse_handler.click_window_with_name('wbtnMirage')
+    await safe_click_window_with_name(client, 'wbtnMirage')
     await asyncio.sleep(0.1)
-    await client.mouse_handler.click_window_with_name('teleportButton')
+    await safe_click_window_with_name(client, 'teleportButton')
     await client.wait_for_zone_change()
     # Collecting wisps
     while (await client.stats.current_hitpoints() < await client.stats.max_hitpoints()) and await client.get_health_wisps():
@@ -115,14 +117,25 @@ async def collect_wisps(client):
     await client.send_key(Keycode.PAGE_DOWN, 0.2)
 
 async def low_collect_wisps(client):
-    # Head to start of world
+    # Head to home world gate
+    await client.send_key(Keycode.HOME, 0.1)
+    await client.wait_for_zone_change()
+    while not await client.is_in_npc_range():
+        await client.send_key(Keycode.S, 0.1)
+    await client.send_key(Keycode.X, 0.1)
+    await asyncio.sleep(0.5)
+    # Go to Dragonspyre
+    await safe_click_window_with_name(client, 'rightButton')
     await asyncio.sleep(0.1)
-    await client.send_key(Keycode.END, 0.1)
+    await safe_click_window_with_name(client, 'wbtnDragonspyre')
+    await asyncio.sleep(0.1)
+    await safe_click_window_with_name(client, 'teleportButton')
+    await client.wait_for_zone_change()
     # Recover
-    while await client.stats.current_hitpoints() < await client.stats.max_hitpoints():
+    while await client.stats.current_hitpoints() < await client.stats.max_hitpoints() and await client.get_health_wisps():
         await safe_tp_to_health(client)
         await asyncio.sleep(0.4)
-    while await client.stats.current_mana() < await client.stats.max_mana():
+    while await client.stats.current_mana() < await client.stats.max_mana() and await client.get_mana_wisps():
         await safe_tp_to_mana(client)
         await asyncio.sleep(0.4)
     # Return
@@ -130,16 +143,16 @@ async def low_collect_wisps(client):
     await client.wait_for_zone_change()
     await client.send_key(Keycode.PAGE_DOWN, 0.2)
 
-
 async def decide_heal(client):
     if await client.needs_potion(health_percent=20, mana_percent=5):
         print(f'[{client.title}] Health is at {round((await client.calc_health_ratio()* 100), 2)}% and mana is at {round((await client.calc_mana_ratio() * 100), 2)}%. Need to recover.')
-        if await client.stats.current_gold() >= 25000 and await client.stats.reference_level() > 5: 
+        if await client.stats.current_gold() >= 25000 and await client.stats.reference_level() >= 5: 
             print(f"[{client.title}] Enough gold, buying potions")
             await auto_buy_potions(client)
         elif await client.stats.reference_level() >= 110:
             print(f"[{client.title}] Low gold, collecting wisps")
             await collect_wisps(client)
-        else:
+        elif await client.stats.reference_level() >= 40:
             print(f"[{client.title}] Collecting wisps")
             await low_collect_wisps(client)
+
