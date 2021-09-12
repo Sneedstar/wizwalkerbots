4d6961be5c9969502474361275ab539ce54c7109
MajorPain1 <51036268+MajorPain1@users.noreply.github.com>
2021-09-12 15:02:43 -0500

    Add files via upload

--- WizFighter/__main__.py
+++ WizFighter/__main__.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 import wizwalker
 from wizwalker.combat import CombatHandler, CombatMember, CombatCard
 from wizwalker import ClientHandler
+from wizwalker.memory.memory_objects.spell_effect import EffectTarget
 
 async def get_spells_by_type_name(client, type_name: str) -> list[wizwalker.combat.CombatCard]:
   async def _pred(card):
@@ -118,7 +119,7 @@ class HighLevelCombat(CombatHandler):
     print("---------------------------------------")
 
     # Heals
-    if ((await client_member.health() / await client_member.max_health()) < .15) and (heals := await get_spells_by_type_name(self, "heal")):
+    if ((await client_member.health() / await client_member.max_health()) < 1.1) and (heals := await get_spells_by_type_name(self, "heal")):
       await asyncio.sleep(0.3)
       # Checking for already enchanted spells
       enchanted_heals = await already_enchanted(heals)
@@ -135,14 +136,12 @@ class HighLevelCombat(CombatHandler):
         to_cast = enchanted_heals[0]
       await asyncio.sleep(0.3)
       final_cast = to_cast
-      target = client_member
     
     # Prisms
     if (is_boss) and (final_cast == "empty"):
       if (await get_school_template_name(self, boss) == await get_school_template_name(self, client_member)) and (prisms := await get_card_vaguely_named(self, "prism")):
         await asyncio.sleep(0.3)
         final_cast = prisms[0]
-        target = boss
 
     # Charms
     if (charms := await get_spells_by_type_name(self, "charm")) and (final_cast == "empty"):
@@ -162,7 +161,6 @@ class HighLevelCombat(CombatHandler):
         to_cast = enchanted_charms[0]
       await asyncio.sleep(0.3)
       final_cast = to_cast
-      target = client_member
 
     # Wards
     if (wards := await get_spells_by_type_name(self, "ward")) and (is_boss) and (final_cast == "empty"):
@@ -182,43 +180,21 @@ class HighLevelCombat(CombatHandler):
         to_cast = enchanted_wards[0]
       await asyncio.sleep(0.3)
       final_cast = to_cast
-      target = boss
 
     # Auras
     if (auras := await get_spells_by_type_name(self, "aura")) and (final_cast == "empty"):
       await asyncio.sleep(0.5)
       final_cast = auras[0]
-      target = None
 
     # Globals
     if (globals := await get_spells_by_type_name(self, "global")) and (final_cast == "empty"):
       await asyncio.sleep(0.5)
       final_cast = globals[0]
-      target = None
-
-    # AOEs
-    if (aoes := await get_spells_by_type_name(self, "aoe")) and (final_cast == "empty"):
-      await asyncio.sleep(0.3)
-      # Checking for already enchanted spells
-      enchanted_aoes = await already_enchanted(aoes)
-      if enchanted_aoes:
-        to_cast = await highest_pip_spell(enchanted_aoes)
-      else:
-        to_cast = await highest_pip_spell(aoes)
-      # Enchanting
-      if (enchants := await self.get_damage_enchants()) and (not enchanted_aoes) and (not await to_cast.is_item_card()):
-        print(f"Enchanting {await to_cast.display_name()} with {await enchants[0].display_name()}")
-        await enchants[0].cast(to_cast)
-        await asyncio.sleep(0.2)
-        enchanted_aoes = await already_enchanted(await get_spells_by_type_name(self, "aoe"))
-        to_cast = enchanted_aoes[0]
-      await asyncio.sleep(0.3)
-      final_cast = to_cast
-      target = None
 
     # Damages
-    if (damages := await get_spells_by_type_name(self, "damage")) and (final_cast == "empty"):
+    if (await get_spells_by_type_name(self, "damage") or await get_spells_by_type_name(self, "steal") or await get_spells_by_type_name(self, "aoe")) and (final_cast == "empty"):
       await asyncio.sleep(0.3)
+      damages = await get_spells_by_type_name(self, "damage") + await get_spells_by_type_name(self, "steal") + await get_spells_by_type_name(self, "aoe")
       # Checking for already enchanted spells
       enchanted_damages = await already_enchanted(damages)
       if enchanted_damages:
@@ -234,7 +210,25 @@ class HighLevelCombat(CombatHandler):
         to_cast = enchanted_damages[0]
       await asyncio.sleep(0.3)
       final_cast = to_cast
-      target = to_kill
+
+    # Targeting
+    target_effects = []
+    effects = await final_cast.get_spell_effects()
+    for effect in effects:
+      target_effects.append(await effect.effect_target())
+
+    for target_effect in target_effects:
+      if target_effect is EffectTarget.target_global or EffectTarget.self or EffectTarget.enemy_team or EffectTarget.friendly_team:
+        target = None
+    for target_effect in target_effects:
+      if (target_effect is EffectTarget.enemy_single) and ((await final_cast.type_name() == "Ward") or ("prism" in await final_cast.display_name())) and (is_boss):
+        target = boss
+    for target_effect in target_effects:
+      if target_effect is EffectTarget.friendly_single:
+        target = client_member
+    for target_effect in target_effects:
+      if target_effect is EffectTarget.enemy_single:
+        target = to_kill
 
     # Casting
     if final_cast:
